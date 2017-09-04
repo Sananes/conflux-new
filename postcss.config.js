c02a960a93a03d40a16b3a1ebb76d75a6608e3c2
Aaron Sananes <aasananes@gmail.com>
2017-09-04 20:18:58 +0100

    Turned to normal scss code

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  parser: 'sugarss',
+  parser: 'postcss-scss',
   plugins: [
-    require('precss')()
+    require('precss')(),
     require('postcss-easy-import')(),
+    require('postcss-custom-media')(),
     require('postcss-simple-vars'),
     require('postcss-cssnext')(),
     require('lost')()
