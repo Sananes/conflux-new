203d1a8d820717ad2785edf63a9d4101614324e3
Aaron Sananes <aasananes@gmail.com>
2017-09-05 16:29:51 +0100

    Big update to the homepage

--- src/data/home.js
+++ src/data/home.js
@@ -1,4 +1,8 @@
 module.exports = {
   title: 'Turn your users\' thoughts into a product',
-  leader: 'Conflux helps you collect feedback, organize it and turn it into a product your customers will love.'
+
+  leader: 'Conflux helps you collect feedback, organize it and turn it into a product your customers will love.',
+
+  featureTitle: 'All-in-one Feedback Management',
+  featureSubtitle: 'Everything you need to understand about your customers'
 };
