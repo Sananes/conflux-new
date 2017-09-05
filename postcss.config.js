f21edc17f0bbf3f314283bb954ba9dbe01a73f74
Aaron Sananes <aasananes@gmail.com>
2017-09-05 07:57:33 +0100

    Added breakpoints, cleared up package.json

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
   plugins: [
     require('precss')(),
     require('postcss-easy-import')(),
-    require('postcss-custom-media')(),
     require('postcss-simple-vars'),
     require('postcss-cssnext')(),
     require('lost')()
